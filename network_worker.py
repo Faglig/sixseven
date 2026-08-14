9caf611f7d415911e84b685b4b32512795c539f0
Faglig <88788611+Faglig@users.noreply.github.com>
2026-08-14 09:05:07 +0300

    Add files via upload

--- network_worker.py
+++ network_worker.py
@@ -1,8 +1,10 @@
+# file: network_worker.py
+
 import requests
 from PyQt5.QtCore import QThread, pyqtSignal
 
 class DownloadWorker(QThread):
-    data_loaded = pyqtSignal(object)   # <-- object вместо dict
+    data_loaded = pyqtSignal(object)  # object - может быть str, dict, list
     error = pyqtSignal(str)
 
     def __init__(self, url):
@@ -13,7 +15,15 @@ class DownloadWorker(QThread):
         try:
             response = requests.get(self.url, timeout=10)
             response.raise_for_status()
-            data = response.json()
+            
+            content_type = response.headers.get('content-type', '')
+            
+            if 'json' in content_type:
+                data = response.json()
+            else:
+                # Для .md файлов - просто текст
+                data = response.text
+            
             self.data_loaded.emit(data)
         except Exception as e:
             self.error.emit(str(e))
